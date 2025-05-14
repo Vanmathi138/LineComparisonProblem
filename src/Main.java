dd6ef68b259669981bde53279e0647f845c4f805
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2025-05-14 15:45:48 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start with Displaying\n" +
+                "Welcome to Line\n" +
+                "Comparison Computation\n" +
+                "Program on Master Branch");
+    }
+}
